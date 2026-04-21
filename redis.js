801abe49359cb1fa13f98245a36b7a9a61bd8b2b
divrawat2 <divrawat2001second@gmail.com>
2026-04-21 23:11:05 +0530

    fe0rg5erg

--- redis.js
+++ redis.js
@@ -1,17 +1,16 @@
-// config/redis.js
 import { createClient } from "redis";
 import "dotenv/config.js";
 
 const redisClient = createClient({
     socket: {
         host: process.env.REDIS_HOST,
-        port: Number(process.env.REDIS_PORT),
+        port: 6379,
+        tls: true, // 🔥 REQUIRED for Valkey Serverless
     },
 });
 
-redisClient.on("error", (err) => {
-    console.log("Redis Error:", err);
-});
+redisClient.on("error", (err) => console.log("Redis Error:", err));
+redisClient.on("connect", () => console.log("Redis connected"));
 
 await redisClient.connect();
 
