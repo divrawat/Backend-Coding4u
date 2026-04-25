840948af7dd9e2c3cd96e114c41fb8be39486ea7
divrawat2 <divrawat2001second@gmail.com>
2026-04-25 22:01:54 +0530

    gv0erwbg5er

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use('/api', formRoutes);
 app.use('/api', ImageRoutes);
 app.use('/api', storyRoutes);
 
-app.get('/', (req, res) => { res.json("Backend index 00000 - Jenkins Auto Deploy Divyanshu"); });
+app.get('/', (req, res) => { res.json("Backend index 00 - Jenkins Auto Deploy Divyanshu"); });
 const port = process.env.PORT || 8000;
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => { console.log(`Server is running on port ${port}`); });
--- tests/sample.test.js
+++ tests/sample.test.js
@@ -5,6 +5,6 @@ describe("GET /", () => {
     it("should return backend message", async () => {
         const res = await request(app).get("/");
         expect(res.statusCode).toBe(200);
-        expect(res.body).toBe("Backend index 00000 - Jenkins Auto Deploy Divyanshu");
+        expect(res.body).toBe("Backend index 00 - Jenkins Auto Deploy Divyanshu");
     });
 });
